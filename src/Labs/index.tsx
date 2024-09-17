738debf8b1a3ade92f5b2be9e58a82b132e18bb6
Ankur Baliga <ankurbaliga@macbookair.mynetworksettings.com>
2024-09-17 01:08:47 -0400

    added my github repo link and my full name

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -3,17 +3,29 @@ import { Route, Routes, Navigate } from "react-router";
 import TOC from "./TOC";
 import Lab3 from "./Lab3";
 import Lab2 from "./Lab2";
+
 export default function Labs() {
   return (
     <div id="wd-labs">
       <h1>Labs</h1>
+      <p>Mangalpady Ankur Baliga</p>
+      <a
+        id="wd-github"
+        href="https://github.com/ankurbaliga8/kanbas-react-web-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        My GitHub Repository
+      </a>
+
       <TOC />
+
       <Routes>
         <Route path="/" element={<Navigate to="Lab1" />} />
+        <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2" element={<Lab2 />} />
         <Route path="Lab3" element={<Lab3 />} />
       </Routes>
-      <Lab1 />
     </div>
   );
 }
